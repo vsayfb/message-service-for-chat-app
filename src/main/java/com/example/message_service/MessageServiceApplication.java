c1445ef9687da747c338b347f6ea56aa887da55c
vsay <96926401+vsayfb@users.noreply.github.com>
2024-06-12 10:06:02 -0400

    create project

---
+++ src/main/java/com/example/message_service/MessageServiceApplication.java
@@ -0,0 +1,26 @@
+package com.example.message_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageServiceApplication.class, args);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
