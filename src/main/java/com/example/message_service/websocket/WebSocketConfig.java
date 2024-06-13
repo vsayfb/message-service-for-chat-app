d01ab8d5104c02a2d5a8f4609dcfc112687fadef
vsay <96926401+vsayfb@users.noreply.github.com>
2024-06-14 01:01:48 +0300

    enable rabbit-stomp broker & test connection

--- src/main/java/com/example/message_service/websocket/WebSocketConfig.java
+++ src/main/java/com/example/message_service/websocket/WebSocketConfig.java
@@ -1,6 +1,8 @@
 package com.example.message_service.websocket;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -9,8 +11,22 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${stomp.broker.host:localhost}")
+    private String host;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("rooms");
+        registry.addEndpoint("rooms").setAllowedOrigins("*");
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+        registry.setApplicationDestinationPrefixes("messages")
+                .enableStompBrokerRelay("/topic")
+                .setRelayHost(host)
+                .setRelayPort(61613)
+                .setClientLogin("guest")
+                .setClientPasscode("guest");
     }
 }
--- src/test/java/com/example/message_service/websocket/WebSocketConfigIntTest.java
+++ src/test/java/com/example/message_service/websocket/WebSocketConfigIntTest.java
@@ -3,6 +3,8 @@ package com.example.message_service.websocket;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.messaging.converter.StringMessageConverter;
+import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSessionHandler;
 import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
@@ -10,10 +12,13 @@ import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 
+import java.lang.reflect.Type;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,6 +31,52 @@ public class WebSocketConfigIntTest {
 
     @Test
     void testConnection() throws ExecutionException, InterruptedException, UnknownHostException {
+        StompSession session = connectWebsocket();
+
+        assertTrue(session.isConnected());
+
+        session.disconnect();
+
+        assertFalse(session.isConnected());
+    }
+
+    @Test
+    void testRelayBroker() throws ExecutionException, InterruptedException, UnknownHostException {
+        StompSession session = connectWebsocket();
+
+        assertTrue(session.isConnected());
+
+        String message = "hello broker";
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        session.subscribe("/topic/room-1", new StompSessionHandlerAdapter() {
+
+            @Override
+            public Type getPayloadType(StompHeaders headers) {
+                return String.class;
+            }
+
+            @Override
+            public void handleFrame(StompHeaders headers, Object payload) {
+                String received = (String) payload;
+
+                assertEquals(received, message);
+
+                latch.countDown();
+            }
+        });
+
+        session.send("/topic/room-1", message);
+
+        boolean received = latch.await(2, TimeUnit.SECONDS);
+
+        if (!received) {
+            throw new RuntimeException("Message was not received. Broker may be not connected");
+        }
+    }
+
+    StompSession connectWebsocket() throws ExecutionException, InterruptedException, UnknownHostException {
 
         String host = InetAddress.getLocalHost().getHostAddress();
 
@@ -38,18 +89,14 @@ public class WebSocketConfigIntTest {
 
         WebSocketStompClient stompClient = new WebSocketStompClient(webSocketClient);
 
-        String url =  String.format("ws://%s:%s/%s", host, port, wsEndpoint);
+        stompClient.setMessageConverter(new StringMessageConverter());
+
+        String url = String.format("ws://%s:%s/%s", host, port, wsEndpoint);
 
         StompSessionHandler sessionHandler = new StompSessionHandlerStub();
 
         CompletableFuture<StompSession> asyncSession = stompClient.connectAsync(url, sessionHandler);
 
-        StompSession session = asyncSession.get();
-
-        assertTrue(session.isConnected());
-
-        session.disconnect();
-
-        assertFalse(session.isConnected());
+        return asyncSession.get();
     }
 }
