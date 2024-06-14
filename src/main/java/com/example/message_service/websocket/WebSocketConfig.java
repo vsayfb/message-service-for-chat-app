c6d03b579ccf94ca81c24ed372380a60547ad669
vsay <96926401+vsayfb@users.noreply.github.com>
2024-06-14 15:46:12 +0300

    implement subscription interceptor

--- src/main/java/com/example/message_service/websocket/WebSocketConfig.java
+++ src/main/java/com/example/message_service/websocket/WebSocketConfig.java
@@ -1,7 +1,9 @@
 package com.example.message_service.websocket;
 
+import com.example.message_service.websocket.interceptor.SubscriptionInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -11,16 +13,29 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    private final SubscriptionInterceptor subscriptionInterceptor;
+
     @Value("${stomp.broker.host:localhost}")
     private String host;
+
+    public WebSocketConfig(SubscriptionInterceptor authenticationInterceptor) {
+        this.subscriptionInterceptor = authenticationInterceptor;
+    }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("rooms").setAllowedOrigins("*");
+
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(subscriptionInterceptor);
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+
 
         registry.setApplicationDestinationPrefixes("messages")
                 .enableStompBrokerRelay("/topic")
---
+++ src/main/java/com/example/message_service/websocket/interceptor/SubscriptionInterceptor.java
@@ -0,0 +1,45 @@
+package com.example.message_service.websocket.interceptor;
+
+import com.example.message_service.external.ExternalRoomService;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SubscriptionInterceptor implements ChannelInterceptor {
+
+    private  final ExternalRoomService externalRoomService;
+
+    public SubscriptionInterceptor(ExternalRoomService externalRoomService) {
+        this.externalRoomService = externalRoomService;
+    }
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message);
+
+        if (StompCommand.SUBSCRIBE.equals(headerAccessor.getCommand())) {
+            String dest = headerAccessor.getDestination();
+
+            if (dest == null || !dest.startsWith("/topic/")) {
+                return null;
+            }
+
+            String roomId = dest.split("/")[2];
+
+            if (roomId == null || roomId.isEmpty()) {
+                return null;
+            }
+
+            if(!externalRoomService.checkRoomExists(roomId)){
+                return null;
+            }
+        }
+
+        return message;
+    }
+}
