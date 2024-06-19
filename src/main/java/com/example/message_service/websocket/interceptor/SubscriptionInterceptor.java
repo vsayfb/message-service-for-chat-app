5b93acd293e7fed4ca121bcefcb175d26181b5be
vsay <96926401+vsayfb@users.noreply.github.com>
2024-06-19 19:29:00 +0300

    publish join information to the topic when user subscribes a room

--- src/main/java/com/example/message_service/websocket/interceptor/SubscriptionInterceptor.java
+++ src/main/java/com/example/message_service/websocket/interceptor/SubscriptionInterceptor.java
@@ -1,10 +1,15 @@
 package com.example.message_service.websocket.interceptor;
 
+import com.example.message_service.dto.RoomMember;
+import com.example.message_service.dto.RoomMessage;
+import com.example.message_service.dto.RoomMessageAction;
 import com.example.message_service.external.ExternalRoomService;
 import com.example.message_service.external.dto.NewMemberResponse;
 import com.example.message_service.jwt.JWTValidator;
 import com.example.message_service.jwt.claims.JWTClaims;
+import com.example.message_service.publisher.RoomMessagePublisher;
 
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.messaging.Message;
@@ -20,11 +25,15 @@ public class SubscriptionInterceptor implements ChannelInterceptor {
 
     private final ExternalRoomService externalRoomService;
 
+    private final RoomMessagePublisher roomMessagePublisher;
+
     private JWTValidator jwtValidator;
 
-    public SubscriptionInterceptor(ExternalRoomService externalRoomService, JWTValidator jwtValidator) {
+    public SubscriptionInterceptor(ExternalRoomService externalRoomService, JWTValidator jwtValidator,
+            RoomMessagePublisher roomMessagePublisher) {
         this.externalRoomService = externalRoomService;
         this.jwtValidator = jwtValidator;
+        this.roomMessagePublisher = roomMessagePublisher;
     }
 
     @Override
@@ -65,5 +74,44 @@ public class SubscriptionInterceptor implements ChannelInterceptor {
         }
 
         return message;
+    }
+
+    @Override
+    public void afterSendCompletion(Message<?> message, MessageChannel channel, boolean sent, Exception ex) {
+
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message);
+
+        if (StompCommand.SUBSCRIBE.equals(headerAccessor.getCommand())) {
+
+            Map<String, Object> sessionAttributes = headerAccessor.getSessionAttributes();
+
+            if (sessionAttributes.isEmpty()) {
+                return;
+            }
+
+            try {
+                NewMemberResponse websocketSession = (NewMemberResponse) sessionAttributes.get("user");
+
+                RoomMessage roomMessage = new RoomMessage();
+
+                roomMessage.setDestination("/topics/" + websocketSession.getRoomId());
+
+                RoomMember roomMember = new RoomMember();
+
+                roomMember.setMemberId(websocketSession.getMemberId());
+                roomMember.setUserId(websocketSession.getUserId());
+                roomMember.setUsername(websocketSession.getUsername());
+
+                RoomMessageAction<RoomMember> messageAction = new RoomMessageAction<>();
+
+                messageAction.setType(RoomMessageAction.Type.JOIN);
+                messageAction.setSubject(roomMember);
+
+                roomMessagePublisher.publish(roomMessage);
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+
+        }
     }
 }
