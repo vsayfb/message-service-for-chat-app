bc7a6a952d0f4fb84e7fde216083478c5138ed88
vsay <96926401+vsayfb@users.noreply.github.com>
2024-06-19 19:06:41 +0300

    create RoomMessagePublisher

---
+++ src/main/java/com/example/message_service/publisher/RoomMessagePublisher.java
@@ -0,0 +1,20 @@
+package com.example.message_service.publisher;
+
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+import com.example.message_service.dto.RoomMessage;
+
+@Component
+public class RoomMessagePublisher {
+
+    private final SimpMessagingTemplate template;
+
+    public RoomMessagePublisher(SimpMessagingTemplate template) {
+        this.template = template;
+    }
+
+    void publish(RoomMessage message) {
+        template.convertAndSend(message.getDestination(), message);
+    }
+}
